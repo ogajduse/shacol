f47ab6c1c1641aa4626bb3a113ec7162565182c8
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-03-07 21:57:39 +0100

    simplify statements

--- sizeInMemory.py
+++ sizeInMemory.py
@@ -1,10 +1,11 @@
 bitcount = 2
 triesize = 2
-for i in range(2, 70):
+for i in range(2, 101):
     triesize = triesize + 2 ** i
     arraysize = (2 ** bitcount) * bitcount
     print "Bits:  ", bitcount
-    print "TRIE:  ", triesize, "b, ", triesize / 8 / 1024 / 1024 / 1024, "GB"
-    print "ARRAY: ", arraysize, "b, ", arraysize / 8 / 1024 / 1024 / 1024, "GB"
+    print "TRIE:  ", triesize, "b, ", triesize / 8 / (1024**3), "GB"
+    print "ARRAY: ", arraysize, "b, ", arraysize / 8 /(1024**3), "GB"
+    print "You will save with trie: ", (arraysize - triesize)/8/(1024**3), "GB"
     bitcount += 1
     print "_______________________________________"
