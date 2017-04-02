2314e8cb90bd226ea91be84c6d2dc34c8659dbae
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-04-02 16:04:15 +0200

    HTML template, Django test
    Edited HTML template and Django test for others test methods

--- tests/django_test.py
+++ tests/django_test.py
@@ -5,33 +5,50 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__))+"/../")
 import shacol
 db_location = os.path.dirname(os.path.abspath(__file__))+"/../app/aplikace/db.sqlite3"
 git_repo = r = git.repo.Repo(os.path.dirname(os.path.abspath(__file__))+"/../")
-sha256 = True
-BITS = 32
-inputFile = "hash.txt"
 
-shacol = shacol.Shacol(sha256, BITS, inputFile)
-db_conn = sqlite3.connect(db_location)
-print(db_conn)
-for i in range(4, 57, 4):
-    shacol.changeBitLength(16)
-    shacol.getInfo()
+def main():
+    sha256 = True
+    BITS = 32
+    inputFile = "hash.txt"
+    shacolInstance = shacol.Shacol(sha256, BITS, inputFile)
 
-    results = shacol.findCollisionStr()
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    print(results)
-    results["index"] = i
-    results["bits"] = i
+    for i in range(4, 33, 4):
+        shacolInstance.changeBitLength(i)
+        shacolInstance.getInfo()
+        results = shacolInstance.findCollisionStr()
+        method = "String method"
+        dbInsert(results, method, i)
+
+        results = shacolInstance.findCollisionInt()
+        method = "Int method"
+        dbInsert(results, method, i)
+
+        #results = shacolInstance.findCollisionWithDBSet()
+        #method = "Method with DB Set"
+        #dbInsert(results, method, i)
 
 
-    #print("INT METODA:::")
-    #shacol.findCollisionInt()
-    #shacol.findCollisionWithDBSet()
+            #print("INT METODA:::")
+            #shacol.findCollisionInt()
+            #shacol.findCollisionWithDBSet()
+
+def dbInsert(results, method, bits):
+    db_conn = sqlite3.connect(db_location)
     db_conn.execute("INSERT INTO website_collision (hash_order, input_hash, total_time, cycles, coll_hash, test_method, bits, git_revision) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    (results["index"], results["inputHash"], results["time"], results["cycles"], results["collisionHash"], "String method", results["bits"], git_repo.git.describe()))
+                    (results["indexOfCollision"], results["inputHash"], results["time"], results["cycles"], results["collisionHash"], method, bits, git_repo.git.describe()))
     db_conn.commit()
     print("Pridano do databaze")
+    db_conn.close()
 
-db_conn.close()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('\nInterrupted... Terminating')
+        sys.exit()
+
+#db_conn.close()
 
 
 """
