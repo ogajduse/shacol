f73b2e2c4396cc65554ab28ac2cf96dd35927d99
Ondrej Gajdusek <ogajduse@redhat.com>
2017-04-14 15:31:38 +0200

    change backend from sqlite to mysql

--- app/aplikace/aplikace/settings.py
+++ app/aplikace/aplikace/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'aplikace.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shacol_django',
+        'USER': 'shacol_django_u',
+        'PASSWORD': 'Aim4Uusoom9ea8',
+        'HOST': '85.255.0.154',
+        'PORT': '3306',
     }
 }
 
