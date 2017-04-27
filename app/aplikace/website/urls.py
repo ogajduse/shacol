0196031a3ab3baedce01af6597c0e54879656406
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-04-27 20:00:07 +0200

    Edit url file for graphs

--- app/aplikace/website/urls.py
+++ app/aplikace/website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.colls, name='colls'),
     url(r'^filtering/$', views.filtering, name='filtering'),
+    url(r'^graphs/$', views.graphs, name='graphs'),
     url(r'^delete/$', views.delete, name='delete'),
 ]
