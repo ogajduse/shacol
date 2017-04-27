aaab00484ae335e73deb5bc802716f8b18f3766d
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-04-27 20:00:46 +0200

    Edit views file - Django graphos

--- app/aplikace/website/views.py
+++ app/aplikace/website/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Collision
-import sqlite3
+#import sqlite3
+#from graphos.sources.model import ModelDataSource
+#from graphos.renderers import flot
+from graphos.sources.simple import SimpleDataSource
+from graphos.renderers.gchart import LineChart
+
 # Create your views here.
 def colls(request):
     collisions = Collision.objects.all()
-    return render(request, 'website/base.html', {'collisions': collisions})
+    return render(request, 'website/collision.html', {'collisions': collisions})
+
+def graphs(request):
+    data =  [
+        ['Total time', 'Bits'],
+        [2004, 1000],
+        [2005, 1170],
+        [2006, 660],
+        [2007, 1030]
+    ]
+    # DataSource object
+    data_source = SimpleDataSource(data=data)
+    # Chart object
+    chart = LineChart(data_source)
+    context = {'chart': chart}
+    return render(request, 'website/graphs.html', context)
+    """collisions = Collision.objects.all()
+    data = ModelDataSource(collisions, fields=['bits', 'total_time'])
+    chart = flot.LineChart(data)
+    return render(request, 'website/graphs.html', {'chart: chart})"""
 
 def delete(request):
     colls = Collision.objects.all()
@@ -32,7 +56,7 @@ def filtering(request):
             colls = colls.filter(test_method__contains="db")
             query['method']= request.POST['method']
 
-        return render(request, 'website/base.html', {'colls':colls, 'query':query})
+        return render(request, 'website/collision.html', {'colls':colls, 'query':query})
 
     else:
         return redirect('/')
