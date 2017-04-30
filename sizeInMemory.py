6915465cc378d70728f556a34aee8e478d9b243d
Ondrej Gajdusek <ogajduse@redhat.com>
2017-04-30 22:31:55 +0200

    nicer output

--- sizeInMemory.py
+++ sizeInMemory.py
@@ -3,14 +3,9 @@ triesize = 2
 for i in range(2, 101):
     triesize = triesize + 2 ** i
     arraysize = (2 ** bitcount) * bitcount
-    print
-    "Bits:  ", bitcount
-    print
-    "TRIE:  ", triesize, "b, ", triesize / 8 / (1024 ** 3), "GB"
-    print
-    "ARRAY: ", arraysize, "b, ", arraysize / 8 / (1024 ** 3), "GB"
-    print
-    "You will save with trie: ", (arraysize - triesize) / 8 / (1024 ** 3), "GB"
+    print("Bits:  " + str(bitcount))
+    print("TRIE:  " + str(triesize) + " b, " + str(triesize / 8 / (1024 ** 3)) + " GB")
+    print("ARRAY: " + str(arraysize) + " b, " + str(arraysize / 8 / (1024 ** 3)) + " GB")
+    print("You will save with trie: " + str((arraysize - triesize) / 8 / (1024 ** 3))+ " GB")
     bitcount += 1
-    print
-    "_______________________________________"
+    print("_______________________________________")
