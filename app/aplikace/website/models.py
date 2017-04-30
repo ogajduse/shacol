dd1cc24736a003b4fd15d6e76c3d2a30b46887fc
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-04-30 15:41:51 +0200

    Edit DB

--- app/aplikace/website/models.py
+++ app/aplikace/website/models.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 
 class Collision(models.Model):
     hash_order = models.IntegerField()
-    input_hash = models.CharField(max_length=30)
-    total_time = models.CharField(max_length=30)
+    input_hash = models.CharField(max_length=60)
+    total_time = models.FloatField()
     cycles = models.IntegerField()
-    coll_hash = models.CharField(max_length=30)
+    coll_hash = models.CharField(max_length=60)
+    total_memory = models.FloatField(default=0.0)
     test_method = models.CharField(max_length=30)
     bits = models.IntegerField()
-    git_revision = models.CharField(max_length=50)
+    git_revision = models.CharField(max_length=30)
 
     def publish(self):
         self.save()
