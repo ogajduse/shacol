22a00176172fd0b4e55222b86d3b20bd635411a5
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-05-01 11:05:38 +0200

    Add new columns for firstTemp, lastTemp

--- app/aplikace/website/models.py
+++ app/aplikace/website/models.py
@@ -3,15 +3,17 @@ from django.utils import timezone
 
 
 class Collision(models.Model):
-    hash_order = models.IntegerField()
-    input_hash = models.CharField(max_length=60)
-    total_time = models.FloatField()
-    cycles = models.IntegerField()
-    coll_hash = models.CharField(max_length=60)
-    total_memory = models.FloatField(default=0.0)
-    test_method = models.CharField(max_length=30)
-    bits = models.IntegerField()
-    git_revision = models.CharField(max_length=30)
+    hash_order = models.IntegerField() #indexOfLast
+    input_hash = models.CharField(max_length=60) #inputHash
+    total_time = models.FloatField()    #time
+    cycles = models.IntegerField()      #cyclesBetCol
+    coll_hash = models.CharField(max_length=60) #collisionHash
+    firstTemp = models.CharField(default=0, max_length=60) #first temp
+    lastTemp = models.CharField(default=0, max_length=60)  #last temp
+    total_memory = models.FloatField(default=0.0) #dataStructConsum
+    test_method = models.CharField(max_length=30)   #method
+    bits = models.IntegerField()    #bits
+    git_revision = models.CharField(max_length=30)  #git_repo.git.describe()
 
     def publish(self):
         self.save()
