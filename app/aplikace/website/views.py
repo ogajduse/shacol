1dfe9401a4e17106100160f26b5dd0da68949db6
Sarka Chwastkova <chwastkova.s@gmail.com>
2017-05-02 19:52:01 +0200

    Remake existing graphs

--- app/aplikace/website/views.py
+++ app/aplikace/website/views.py
@@ -5,131 +5,131 @@ from graphos.sources.simple import SimpleDataSource
 from graphos.renderers.gchart import LineChart
 from django.db.models import Max, Min
 from graphos.sources.model import ModelDataSource
+import copy
 
 def colls(request):
     colls = Collision.objects.all()
     return render(request, 'website/collision.html', {'colls': colls})
 
 def graphs(request):
-    max_bits_str = (Collision.objects.filter(test_method__icontains="Str").aggregate(Max('bits'))['bits__max']) + 1
-    max_bits_int = (Collision.objects.filter(test_method__icontains="Int").aggregate(Max('bits'))['bits__max']) + 1
-    max_bits_db = (Collision.objects.filter(test_method__icontains="db").aggregate(Max('bits'))['bits__max']) + 1
-    max_bit_list = [max_bits_str, max_bits_int, max_bits_db]
-    max_bits = min(max_bit_list)
-    min_bits_str = Collision.objects.filter(test_method__icontains="Str").aggregate(Min('bits'))['bits__min']
-    min_bits_int = Collision.objects.filter(test_method__icontains="Int").aggregate(Min('bits'))['bits__min']
-    min_bits_db = Collision.objects.filter(test_method__icontains="db").aggregate(Min('bits'))['bits__min']
-    min_bit_list = [min_bits_str, min_bits_int, min_bits_db]
-    min_bits = max(min_bit_list)
-
-    data_tt = [['Bits', 'Int method', 'String method', 'DB Set method']]
-    name_tt = "All methods - total time/bits"
-    for b in range(min_bits, max_bits , 4):
-        colls_int = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="Int", bits=b)
-        colls_str = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="String", bits=b)
-        colls_db = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="db", bits=b)
-        for bits, time in colls_int:
-            tmp = [bits, time]
-        for bits, time in colls_str:
-            tmp.append(time)
-        for bits, time in colls_db:
-            tmp.append(time)
-        data_tt.append(tmp)
-
-    data_ho = [['Bits', 'Int method', 'String method', 'DB Set method']]
-    name_ho = "All - index of collison hash/total bits"
-    for b in range(min_bits, max_bits , 4):
-        colls_int = Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="Int", bits=b)
-        colls_str = Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="String", bits=b)
-        colls_db = Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="db", bits=b)
-        for bits, ho in colls_int:
-            tmp = [bits, ho]
-        for bits, ho in colls_str:
-            tmp.append(ho)
-        for bits, ho in colls_db:
-            tmp.append(ho)
-        data_ho.append(tmp)
-
-    data_tm = [['Bits', 'Int method', 'String method', 'DB Set method']]
-    name_tm = "All - index of collison hash/total bits"
-    for b in range(min_bits, max_bits , 4):
-        colls_int = Collision.objects.values_list("bits", "total_memory").filter(test_method__icontains="Int", bits=b)
-        colls_str = Collision.objects.values_list("bits", "total_memory").filter(test_method__icontains="String", bits=b)
-        colls_db = Collision.objects.values_list("bits", "total_memory").filter(test_method__icontains="db", bits=b)
-        for bits, tm in colls_int:
-            tmp = [bits, tm]
-        for bits, tm in colls_str:
-            tmp.append(tm)
-        for bits, tm in colls_db:
-            tmp.append(tm)
-        data_tm.append(tmp)
-
-
     if 'graphmethod' in request.POST:
         if request.POST['graphmethod'] == "str":
-            data_tt = [['Bits', 'Total time [s]']]
-            colls = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="String")
+            bits = Collision.objects.values_list("bits").filter(test_method__icontains="str").distinct()
+            data = [['Bits']]
             name_tt = "String method - total time/bits"
-            for bits, time in colls:
-                data_tt.append([bits, time])
-
-            data_ho = [['Bits', 'Hashes']]
-            colls_ho= Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="String")
             name_ho = "String method - count of cycles/total bits"
-            for bits, hashes in colls_ho:
-                data_ho.append([bits, hashes])
+            # generate values (bits) for the first column
+            for b in bits:
+                data.append([b[0]])
 
-            data_tm = [['Bits', 'Total memory [MB]']]
-            colls_tm = Collision.objects.values_list("bits", "total_memory").filter(test_method__icontains="String")
-            name_tm = "String method - bits/total memory"
-            for bits, memory in colls_tm:
-                data_tm.append([bits, memory])
+            input_hashes = Collision.objects.values_list("input_hash").filter(test_method__icontains="String").distinct()
+            for ih in input_hashes:
+                data[0].append(ih[0])
 
+            data_tt = copy.deepcopy(data)
+            data_ho = copy.deepcopy(data)
+
+            datadict = dict()
+            for i in range(1, len(data)):
+                datadict[data[i][0]] = i
+
+            for inp_hsh in range(1, len(data[0]), 1):
+                selected_in_hash = data[0][inp_hsh]
+                #print("selected_in_hash: " + str(selected_in_hash))
+                bitlist = list()
+                bitlist_select = Collision.objects.values_list("bits").filter(test_method__icontains="Str", input_hash=selected_in_hash)
+                for i in bitlist_select:
+                    bitlist.append(i[0])
+                #print("printing bitlist")
+                #print(bitlist)
+                for bits in bitlist:
+                    #print("actual bits: " + str(bits))
+                    time = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="Str", input_hash=selected_in_hash, bits=bits)
+                    #print(time.values)
+                    cycles = Collision.objects.values_list("bits", "cycles").filter(test_method__icontains="Str", input_hash=selected_in_hash, bits=bits)
+                    #print(cycles.values)
+                    #print("first arg time: " + str(datadict.get(time[0][0])))
+                    #print("data_tt before insert " + str(data_tt))
+                    data_tt[datadict.get(time[0][0])].insert(inp_hsh, time[0][1])
+                    #print("data_tt after insert " + str(data_tt))
+                    #print("data_ho before insert " + str(data_ho))
+                    data_ho[datadict.get(cycles[0][0])].insert(inp_hsh, cycles[0][1])
+                    #print("data_ho after insert " + str(data_ho))
 
         elif request.POST['graphmethod'] == "int":
-            data_tt = [['Bits', 'Total time [s]']]
-            colls = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="Int")
+            bits = Collision.objects.values_list("bits").filter(test_method__icontains="int").distinct()
+            data = [['Bits']]
             name_tt = "Int method - total time/bits"
-            for bits, time in colls:
-                data_tt.append([bits, time])
-
-            data_ho = [['Bits', 'Hashes']]
-            colls_ho= Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="Int")
             name_ho = "Int method - count of cycles/total bits"
-            for bits, hashes in colls_ho:
-                data_ho.append([bits, hashes])
+            for b in bits:
+                data.append([b[0]])
 
-            data_tm = [['Bits', 'Total memory [MB]']]
-            colls_tm = Collision.objects.values_list("bits", "total_memory").filter(test_method__icontains="Int")
-            name_tm = "Int method - bits/total memory"
-            for bits, memory in colls_tm:
-                data_tm.append([bits, memory])
+            input_hashes = Collision.objects.values_list("input_hash").filter(test_method__icontains="Int").distinct()
+            for ih in input_hashes:
+                data[0].append(ih[0])
+
+            data_tt = copy.deepcopy(data)
+            data_ho = copy.deepcopy(data)
+
+            datadict = dict()
+            for i in range(1, len(data)):
+                datadict[data[i][0]] = i
+
+            for inp_hsh in range(1, len(data[0]), 1):
+                selected_in_hash = data[0][inp_hsh]
+                bitlist = list()
+                bitlist_select = Collision.objects.values_list("bits").filter(test_method__icontains="Int", input_hash=selected_in_hash)
+                for i in bitlist_select:
+                    bitlist.append(i[0])
+                for bits in bitlist:
+                    time = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="Int", input_hash=selected_in_hash, bits=bits)
+                    cycles = Collision.objects.values_list("bits", "cycles").filter(test_method__icontains="Int", input_hash=selected_in_hash, bits=bits)
+                    data_tt[datadict.get(time[0][0])].insert(inp_hsh, time[0][1])
+                    data_ho[datadict.get(cycles[0][0])].insert(inp_hsh, cycles[0][1])
 
         elif request.POST['graphmethod'] == "db":
-            data_tt = [['Bits', 'Total time [s]']]
-            colls = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="DB")
+            #max_bits_db = (Collision.objects.filter(test_method__icontains="db").aggregate(Max('bits'))['bits__max']) + 1
+            bits = Collision.objects.values_list("bits").filter(test_method__icontains="db").distinct()
+            data = [['Bits']]
             name_tt = "DB Set method - total time/bits"
-            for bits, time in colls:
-                data_tt.append([bits, time])
-
-            data_ho = [['Bits', 'Hashes']]
-            colls_ho= Collision.objects.values_list("bits", "hash_order").filter(test_method__icontains="db")
             name_ho = "DB Set method - count of cycles/total bits"
-            for bits, hashes in colls_ho:
-                data_ho.append([bits, hashes])
+            for b in bits:
+                data.append([b[0]])
+
+            input_hashes = Collision.objects.values_list("input_hash").filter(test_method__icontains="db").distinct()
+            for ih in input_hashes:
+                data[0].append(ih[0])
+
+            data_tt = copy.deepcopy(data)
+            data_ho = copy.deepcopy(data)
+
+            datadict = dict()
+            for i in range(1, len(data)):
+                datadict[data[i][0]] = i
+
+            for inp_hsh in range(1, len(data[0]), 1):
+                selected_in_hash = data[0][inp_hsh]
+                bitlist = list()
+                bitlist_select = Collision.objects.values_list("bits").filter(test_method__icontains="db", input_hash=selected_in_hash)
+                for i in bitlist_select:
+                    bitlist.append(i[0])
+                for bits in bitlist:
+                    time = Collision.objects.values_list("bits", "total_time").filter(test_method__icontains="db", input_hash=selected_in_hash, bits=bits)
+                    cycles = Collision.objects.values_list("bits", "cycles").filter(test_method__icontains="db", input_hash=selected_in_hash, bits=bits)
+                    data_tt[datadict.get(time[0][0])].insert(inp_hsh, time[0][1])
+                    data_ho[datadict.get(cycles[0][0])].insert(inp_hsh, cycles[0][1])
 
         elif request.POST['graphmethod'] == "all":
             pass
 
-    data_source_tt = SimpleDataSource(data=data_tt)
-    chart_tt = LineChart(data_source_tt, options={'title': name_tt})
+        data_source_tt = SimpleDataSource(data=data_tt)
+        chart_tt = LineChart(data_source_tt, options={'title': name_tt})
 
-    data_source_ho = SimpleDataSource(data=data_ho)
-    chart_ho = LineChart(data_source_ho, options={'title': name_ho})
+        data_source_ho = SimpleDataSource(data=data_ho)
+        chart_ho = LineChart(data_source_ho, options={'title': name_ho})
 
-    data_source_tm = SimpleDataSource(data=data_tm)
-    chart_tm = LineChart(data_source_tm, options={'title': name_tm})
-    return render(request, 'website/graphs.html', {'chart': chart_tt, 'chart2': chart_ho, 'chart3': chart_tm})
+        return render(request, 'website/graphs.html', {'chart': chart_tt, 'chart2': chart_ho})
+    return render(request, 'website/graphs.html')
     #return redirect('/')
 
 def delete(request):
